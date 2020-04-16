c36ed5fbb894e6d771c649830f92a38ffcfb7c23
wry <test@163.cn>
2020-04-16 11:18:41 +0800

    完成生成静态页面的功能，准备调试

--- apps/user/tasks.py
+++ apps/user/tasks.py
@@ -6,11 +6,39 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import reverse
 import time
+from apps.goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from django.template import loader
+from django.conf import settings
+import os
 
 
 @shared_task
 def add_test(num1, num2):
     print(num1+num2)
+
+@shared_task
+def create_static_index_html(request):
+    print('执行生成静态页面的任务')
+    goods_type_list = GoodsType.objects.all()
+    goods_banner_list = IndexGoodsBanner.objects.all().order_by('index')
+    goods_promotion_list = IndexPromotionBanner.objects.all().order_by('-index')
+
+    for goods_type in goods_type_list:
+        type_title_list = IndexTypeGoodsBanner.objects.filter(type=goods_type, display_type=0).order_by('index')
+        type_image_list = IndexTypeGoodsBanner.objects.filter(type=goods_type, display_type=1).order_by('index')
+        goods_type.type_title_list = type_title_list
+        goods_type.type_image_list = type_image_list
+
+    template = loader.get_template('user/index.html')
+    context = {'goods_type_list': goods_type_list,
+               'goods_banner_list': goods_banner_list,
+               'goods_promotion_list': goods_promotion_list,
+               'cart_count': 0}
+    index_content = template.render(context, request)
+
+    with open(os.path.join(settings.BASE_DIR, 'apps/user/templates/user/index_static.html'), 'wb') as f:
+        f.write(index_content)
+    print('生成完毕')
 
 
 @shared_task
