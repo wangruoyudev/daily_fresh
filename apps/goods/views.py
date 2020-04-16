188c19454dc9909fb7826fd557d2dd0a7ec61ac7
wry <test@163.cn>
2020-04-16 17:23:09 +0800

    提交详情页面

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+
+class GoodsDetail(LoginRequiredMixin, View):
+    def get(self, quest):
+        return render(quest, 'goods/detail.html')
