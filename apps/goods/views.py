9a15e976bc751f2de575d10d63e70a3c2f6f1cea
wry <test@163.cn>
2020-04-16 17:37:47 +0800

    debuging05

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+from django.http import HttpResponse
 
 
 class GoodsDetail(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'user/index.html')
+        # return render(request, 'user/index.html')
+        return HttpResponse('detail')
