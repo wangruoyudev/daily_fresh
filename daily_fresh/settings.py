fa31a684aeedb0098c0c7751dd4d66a80927cb85
wry <test@163.cn>
2020-04-16 15:19:47 +0800

    增加一个模板路径

--- daily_fresh/settings.py
+++ daily_fresh/settings.py
@@ -68,7 +68,7 @@ AUTH_USER_MODEL = 'user.User'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'apps/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'apps/templates'), os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
