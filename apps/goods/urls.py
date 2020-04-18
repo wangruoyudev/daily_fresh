f474fa68baf311c86776f36785191be00c06dd2d
wry <test@163.cn>
2020-04-18 18:38:30 +0800

    用分页类的别的方法，自动处理

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -4,6 +4,6 @@ from .views import GoodsDetailView, IndexView, GoodsTypeListView
 app_name = 'goods'
 urlpatterns = [
     path('index', IndexView.as_view(), name='index'),
-    re_path(r'^detail/(?P<goods_id>\d+)/?$', GoodsDetailView.as_view(), name='detail'),
-    re_path(r'^type_list/(?P<type_id>\d+)/(?P<page_num>\d+)/?$', GoodsTypeListView.as_view(), name='list'),
+    re_path(r'^detail/(?P<goods_id>\d+)$', GoodsDetailView.as_view(), name='detail'),
+    re_path(r'^type_list/(?P<type_id>\d+)/(?P<page_num>\d+)$', GoodsTypeListView.as_view(), name='list'),
 ]
