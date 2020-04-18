675e4c13e6526d034e58843175da88b4bcfd6951
wry <test@163.cn>
2020-04-18 14:06:07 +0800

    加入类别列表页面

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from .views import GoodsDetailView, IndexView
+from .views import GoodsDetailView, IndexView, GoodsTypeListView
 
 app_name = 'goods'
 urlpatterns = [
-    re_path(r'^detail/(?P<goods_id>\d+)/?$', GoodsDetailView.as_view(), name='detail'),
     path('index', IndexView.as_view(), name='index'),
+    re_path(r'^detail/(?P<goods_id>\d+)/?$', GoodsDetailView.as_view(), name='detail'),
+    re_path(r'^type_list/(?P<type_id>\d+)/?$', GoodsTypeListView.as_view(), name='list'),
 ]
