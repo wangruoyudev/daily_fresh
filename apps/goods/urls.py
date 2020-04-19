88a877e2ea88546af4b1e84489dce721ee4da347
wry <test@163.cn>
2020-04-19 17:14:13 +0800

    加入添加购物车功能，先读

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import GoodsDetailView, IndexView, GoodsTypeListView
+from .views import GoodsDetailView, IndexView, GoodsTypeListView, AddCartView
 
 app_name = 'goods'
 urlpatterns = [
     path('index', IndexView.as_view(), name='index'),
     re_path(r'^detail/(?P<goods_id>\d+)$', GoodsDetailView.as_view(), name='detail'),
     re_path(r'^type_list/(?P<type_id>\d+)/(?P<page_num>\d+)$', GoodsTypeListView.as_view(), name='list'),
+    re_path(r'^add_cart/(?P<goods_id>\d+)$', AddCartView.as_view(), name='add_cart'),
 ]
