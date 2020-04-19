0e194e1a836f5f9eac11bb647f8f1dda3acb3f38
wry <test@163.cn>
2020-04-19 17:55:47 +0800

    debug02 返回实际缓存中的数据

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,14 +4,14 @@ from .views import LoginView, RegisterView, ActiveAccount, \
 
 app_name = 'user'
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-    path('register', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
     re_path(r'^active_account/(?P<token>.*)/?$', ActiveAccount.as_view(), name='active_account'),
     path('', UserInfo.as_view(), name='info'),
     # path('', userinfo, name='info'),
-    path('order', UserOrder.as_view(), name='order'),
-    path('address', UserAddress.as_view(), name='address'),
-    path('test', TestView.as_view(), name='test'),
+    path('order/', UserOrder.as_view(), name='order'),
+    path('address/', UserAddress.as_view(), name='address'),
+    path('test/', TestView.as_view(), name='test'),
 
 ]
