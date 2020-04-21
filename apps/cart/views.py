ee35967228cc9974c0e5bf77cabbda2783864afc
wry <test@163.cn>
2020-04-21 11:02:23 +0800

    传入购车对象和数量

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django_redis import get_redis_connection
+from apps.goods.models import GoodsSKU
 # Create your views here.
 
 
@@ -12,6 +13,20 @@ class CartInfoView(LoginRequiredMixin, View):
         cart_key = 'cart_id%s' % request.user.id
         cart_list = con.hgetall(cart_key)
         print('====>cart_list:', cart_list)
+        goods_sku_list = list()
+        total_count = 0
+        for key in cart_list:  # 遍历购物车
+            print('key: ', key.decode(), '   -value: ', cart_list[key].decode())
+            try:
+                goods_sku = GoodsSKU.objects.get(id=key.decode())  # 去除购物车的sku对象
+            except GoodsSKU.DoesNotExist:
+                continue
+            goods_sku.cart_count = cart_list[key].decode()
+            goods_sku_list.append(goods_sku)
+            total_count += int(cart_list[key].decode())
+
         context = {
+            'goods_sku_list': goods_sku_list,
+            'total_count': total_count,
         }
         return render(request, 'cart/cart.html', context)
