43e4c90a293f3cf85f9b59f5dd609b24a4a26c0d
wry <test@163.cn>
2020-04-21 10:40:01 +0800

    加入购物车详情页面

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django_redis import get_redis_connection
 # Create your views here.
 
 
-def cart_index(request):
-    return HttpResponse('cart_index')
+class CartInfoView(LoginRequiredMixin, View):
+    def get(self, request):
+        con = get_redis_connection('default')
+        cart_key = 'cart_id%s' % request.user.id
+        cart_list = con.hgetall(cart_key)
+        print('====>cart_list:', cart_list)
+        context = {
+        }
+        return render(request, 'cart/cart.html', context)
