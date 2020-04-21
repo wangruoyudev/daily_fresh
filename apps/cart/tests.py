56ccf52da66fe7433ab1a083b00baefdca6bcda1
wry <test@163.cn>
2020-04-21 11:02:23 +0800

    传入购车对象和数量

--- apps/cart/tests.py
+++ apps/cart/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+dic1 = {b'3': b'4', b'2': b'1', b'5': b'7', b'12': b'28'}
+
+for key in dic1:
+    print('key: ', key.decode(), '   -value: ', dic1[key].decode())
