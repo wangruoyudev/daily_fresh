2724ec9f45bca3becd0e7b54d1f6fed154f13326
wry <test@163.cn>
2020-04-22 10:20:48 +0800

    加入生成订单页面,使用git commit --amend

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import CreateOrderView
 
+app_name = 'order'
 urlpatterns = [
+    path('create_order/', CreateOrderView.as_view(), name='create_order'),
 ]
--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 # Create your views here.
+
+
+class CreateOrderView(LoginRequiredMixin, View):
+    def get(self, request):
+        context = {}
+        return render(request, 'order/place_order.html', context)
+
