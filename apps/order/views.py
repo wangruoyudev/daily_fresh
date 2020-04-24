55b677e37c95d85c2701bcf69cf01785fe4f4018
wry <test@163.cn>
2020-04-24 15:05:06 +0800

    QueryDict 取数组列表，用getlist

--- apps/order/views.py
+++ apps/order/views.py
@@ -9,7 +9,8 @@ from django_redis import get_redis_connection
 class CreateOrderView(LoginRequiredMixin, View):
     def post(self, request):
         print('====>CreateOrderView-post:', request.POST)
-        goods_id_list = request.POST.get('sku_id', None)
+        # goods_id_list = request.POST.get('sku_id', None)
+        goods_id_list = request.POST.getlist('sku_id', [])
         print('===>goods_id_list:', goods_id_list)
         if goods_id_list is not None:
             conn = get_redis_connection('default')
