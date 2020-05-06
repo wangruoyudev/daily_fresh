0233f9a325f69a409b535bb9899c15b18e71195c
wry <test@163.cn>
2020-05-06 17:22:24 +0800

    没匹配上，后最slash改一下匹配规则

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('submit_order/', SubmitOrderView.as_view(), name='submit_order'),
     path('pay_order/', AliPayView.as_view(), name='pay_order'),
     path('query_order/', QueryTradeStatus.as_view(), name='query_order'),
-    re_path(r'^evaluate_order/(?P<order_id>\d+)/', OrderEvaluate.as_view(), name='evaluate_order'),
+    re_path(r'^evaluate_order/(?P<order_id>\d+)/?', OrderEvaluate.as_view(), name='evaluate_order'),
 ]
