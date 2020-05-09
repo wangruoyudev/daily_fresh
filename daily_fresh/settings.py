185ba9493232bacb3eb03b48ffdd29cd05d4a04b
wry <test@163.cn>
2020-05-09 10:35:20 +0800

    改一下静态文件索引的前缀，不占用static这个

--- daily_fresh/settings.py
+++ daily_fresh/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_daily/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
